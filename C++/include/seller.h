c7dab28802e4cbba5713badadbdc048ad0112baa
byx <1141185027@qq.com>
2021-12-21 10:00:11 +0800

    传到include文件夹下面

---
+++ C++/include/seller.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "shopping.h"
+#include "commodity.h"
+#include "shopping_cart.h"
+#include "sign_in.h"
+
+int InitSe();
---
+++ C++/include/shopping.h
@@ -0,0 +1,12 @@
+#ifndef __SHOPPING_H__
+#define __SHOPPING_H__
+
+#include "commodity.h"
+#include "shopping_cart.h"
+#include "sign_in.h"
+#include "seller.h"
+
+int Shopping(std::map<std::string,int> &my_shoppint_cart);
+
+
+#endif
